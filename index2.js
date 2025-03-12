bccbbf32d9317daf8eb510ce316766b6f15620d6
edunovacki <111133454+edunovacki@users.noreply.github.com>
2025-03-11 21:05:39 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,11 @@
+//Solicite ao usuário para digitar o nome;
+//Solicite ao usuário para digitar o sobrenome;
+//Solicite ao usuário para digitar a cidade onde mora;
+//Solicite ao usuário para digitar a idade;
+//Solicite ao usuário para digitar o time do coração.
+//Ao coletar essas informações, apresente uma mensagem longa concatenando todos 
+//os dados forecidos pelo usuário de forma criativa. Por exemplo:
+//"Olá, [NOME][SOBRENOME]! É um prazer conhecer você. Eu vejo que você mora na cidade de [CIDADE].
+// Com [IDADE] anos, você já deve ter muitas histórias para contar. E que interessante
+// saber que você é torcedor(a) do [TIME DO CORAÇÃO]! Espero que seu time tenha muitas
+// vitórias pela frente."
