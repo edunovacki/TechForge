6453a33db88a9203be1da2554e49feecd676b995
edunovacki <111133454+edunovacki@users.noreply.github.com>
2025-03-11 22:42:29 -0300

    Add files via upload

---
+++ ListaExercicio2.js
@@ -0,0 +1,55 @@
+const leitura = require("prompt-sync")();
+
+//1. Leia um valor inteiro e apresente sua metade.
+
+//let valor = leitura("Informe o número: ");
+//let metade = valor/2;
+//console.log("O valor é: "+metade);
+
+//let valor = leitura("Informe o número: ");
+//console.log("O valor é: "+(valor/2));
+
+//2. Leia um valor decimal e apresente a sua divisão por 5.
+//let valor = leitura("Informe um número decimal: ");
+//let divisao = valor/5;
+//console.log("O valor é: "+divisao);
+
+//let valor = leitura("Informe um número decimal: ");
+//console.log("O valor é: "+(valor/5));
+
+//3. Leia um valor decimal (que será o valor) e outro número inteiro (que seja
+//o divisor) e apresente a sua divisão do primeiro número pelo segundo.
+
+//let valorDecimal = leitura("Informe um número decimal: ");
+//let divisor = leitura("Informe o divisor: ");
+//console.log("O valor decimal é "+valorDecimal+", O divisor é "+divisor+", O resultado da divisão do primeiro número pelo segundo é "+(valorDecimal/divisor));
+
+//4. Faça um algoritmo que leia nome, endereço e telefone de uma pessoa e
+//apresente na tela:
+//a. NOME = -------------------------------------
+//b. ENDEREÇO = -------------------------------------
+//c. TELEFONE = -------------------------------------
+
+//let nome = leitura("Informe o seu nome completo: ");
+//let endereco = leitura("Informe o seu endereço: ");
+//let telefone = leitura("Informe o seu telefone: ");
+//console.log("a. NOME = "+nome);
+//console.log("b. ENDEREÇO = "+endereco);
+//console.log("c. TELEFONE = "+telefone);
+
+//5. Faça um algoritmo para que forneça apenas o lado de um triângulo
+//equilátero, seja calculada e retornada a área dessa figura.
+
+//let ladoTrianguloEquilatero = leitura("Informe o lado de um triângulo equilátero: ");
+//let area = ladoTrianguloEquilatero*3;
+//console.log("A área do triângulo equilátero é: "+area);
+
+//6. Elabore um algoritmo que calcule e escreva a área de um círculo.
+//Sabe-se que Área = π * R², onde R é o raio do círculo (lido) e π é o valor
+//de PI (3,1415).
+
+//let raioCirculo = leitura("Informe o raio do círculo: ");
+//let area = 3.1415*raioCirculo*raioCirculo;
+//console.log("A área do círculo é: "+area);
+
+//7. 
